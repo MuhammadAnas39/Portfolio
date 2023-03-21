33f38495cf814013c4b3875591a633c36ca786a0
Anas <anasnizamani54@gmail.com>
2023-03-22 01:49:12 +0500

    Portfolio

---
+++ src/Components/About.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full bg-gradient-to-b from-gray-800
+    to-black text-white pt-20 md:pt-0"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ">
+        <div className="pt-8">
+          <p className="text-4xl font-bold border-b-4 border-gray-500 inline pt-4">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic unde
+          explicabo adipisci nihil dolor et iusto, ratione cum accusantium
+          accusamus at maiores eum quaerat! Laudantium eius autem magni aperiam
+          laboriosam officia quidem optio nemo veniam ad omnis, rerum tempore
+          atque facere sunt mollitia itaque tempora incidunt eveniet. Tenetur,
+          quaerat at?
+        </p>
+        <br />
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla
+          dolorem voluptatem vitae, explicabo possimus pariatur incidunt hic
+          consectetur ab quis labore voluptas harum reprehenderit reiciendis. Ex
+          quo quia aspernatur minus quisquam cupiditate illum possimus ducimus
+          error, eos officiis vel dicta eaque aliquid laboriosam mollitia
+          maxime. Ea saepe animi perferendis.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import firstOne from "../assets/btc.png";
+import me from "../assets/me.jpg";
+
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: firstOne,
+    },
+    {
+      id: 2,
+      src: me,
+    },
+    {
+      id: 3,
+      src: firstOne,
+    },
+    {
+      id: 4,
+      src: me,
+    },
+    {
+      id: 5,
+      src: firstOne,
+    },
+    {
+      id: 6,
+      src: me,
+    },
+  ];
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white min-h-screen py-20"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Checkout some of my work right here</p>
+        </div>
+
+        <div className="grid sm-grid-cols-2 md:grid-cols-3 gap-8 px-10 md:px-0">
+          {portfolios.map(({ src, id }) => (
+            <div
+              key={id}
+              className="shadow-md shadow-gray-600 rounded-lg duration-200 hover:scale-105 "
+            >
+              <img src={src} alt="portfolios" className="rounded-t-lg" />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-md font-bold">
+                  <a href="https://github.com/CodingWithAnas1/My-Crypto-app">
+                    Demo
+                  </a>
+                </button>
+
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 border-blue-500 border-2 rounded-md font-bold">
+                  <a
+                    href="https://my-crypto-app-kappa.vercel.app/"
+                    target="blank"
+                  >
+                    Code
+                  </a>
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+
+    // <div className="w-full min-h-screen bg-gradient-to-b from-black to-gray-800">
+    //   <div className="h-full w-full max-w-screen-lg p-4 mx-auto">
+    //     <div className="">
+    //       <p className="text-4xl text-white border-b-4 inline border-gray-500 font-bold">
+    //         Portfolio
+    //       </p>
+    //     </div>
+
+    //     <div className="flex flex-wrap justify-evenly ">
+    //       {portfolios.map((e) => (
+    //         <div className="m-4 border-2 w-52 flex flex-col ">
+    //           <div className="text-white">
+    //             <img src={me} alt="" className="" />
+    //             <p className="text-center">fsdafsd</p>
+    //             <div className="flex justify-center items-center gap-10">
+    //               <button className="px-3 border-2 my-2 py-4">
+    //                 hellow there
+    //               </button>
+    //               <button className="px-3 border-2 my-2 py-4">
+    //                 hellow there
+    //               </button>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       ))}
+    //     </div>
+    //   </div>
+    // </div>
+  );
+};
+
+export default Portfolio;
